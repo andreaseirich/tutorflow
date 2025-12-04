710fcc80e76e789b2fc064dc2f670dadf8dce09e
andreaseirich <andreas.eirich@iu-study.org>
2025-12-05 00:56:00 +0100

    fix: add URL route to serve invoice documents
    - Add MEDIA_URL and MEDIA_ROOT to settings.py
    - Add serve_invoice_document view to serve invoice HTML files
    - Update invoice_detail template to use new URL route
    - Add static media file serving in development mode
    - Fixes 404 error when accessing invoice documents

--- backend/tutorflow/urls.py
+++ backend/tutorflow/urls.py
@@ -6,6 +6,8 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +20,7 @@ urlpatterns = [
     path('billing/', include('apps.billing.urls')),
     path('ai/', include('apps.ai.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
